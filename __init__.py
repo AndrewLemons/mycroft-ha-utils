56b0be50839e7579bdd5ac775e8007ab7ccd3d50
Andrew <andrewglemons@gmail.com>
2020-08-20 20:21:55 -0400

    Initial

---
+++ __init__.py
@@ -0,0 +1,38 @@
+from mycroft.skills.core import MycroftSkill
+from mycroft.skills.audioservice import AudioService
+
+import requests
+
+class HAUtils(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(HAUtils, self).__init__(name="HomeAssistantUtils")
+
+    def initialize(self):
+        self.audio_service = AudioService(self.bus)
+
+        self.host_address = self.settings.get('host_address')
+        self.llat = self.settings.get('llat')
+        self.lightEntity = self.settings.get('light_entity')
+
+        self.register_intent_file('lights.intent', self.handle_lights)
+
+    def handle_lights(self, message):
+        state = message.data.get('state')
+
+        method = "http"
+        if self.settings.get('ssl') == "true":
+            method = "https"
+        
+        url = method + "://" + self.settings.get('host_address') + ":" + self.settings.get('port') + "/api/states/" + self.settings.get('light_entity')
+        payload = {"state": state}
+        requests.post(url, data = payload, headers = {"Authorization": "Bearer " + self.settings.get('llat')})
+
+        self.speak("the lights have been turned " + state)
+
+    def stop(self):
+        pass
+
+def create_skill():
+    return HAUtils()
